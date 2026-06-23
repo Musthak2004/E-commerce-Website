cca15b2054ef9ed71be6edbeada83f1f91115d66
Musthak <musthakcool55@gmail.com>
2026-06-23 21:38:43 +0530

    Add seller-only product management views and update URLs

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,24 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
+
+app_name = "products"
+
+urlpatterns = [
+
+    path("", ProductListView.as_view(), name="product_list"),
+
+    path("create/", ProductCreateView.as_view(), name="product_create"),
+
+    path("<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
+
+    path("<slug:slug>/update/", ProductUpdateView.as_view(), name="product_update"),
+
+    path("<slug:slug>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,121 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.urls import reverse_lazy
+
+from django.shortcuts import get_object_or_404
+
+from .models import Product
+from .forms import ProductForm
+
+
+class SellerRequiredMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.user_type == "SELLER"
+
+
+class ProductListView(ListView):
+
+    model = Product
+
+    template_name = "products/product_list.html"
+
+    context_object_name = "products"
+
+    paginate_by = 12
+
+    def get_queryset(self):
+
+        return Product.objects.filter(
+            is_available=True
+        ).select_related(
+            "seller", "category"
+        )
+
+
+class ProductDetailView(DetailView):
+
+    model = Product
+
+    template_name = "products/product_detail.html"
+
+    context_object_name = "product"
+
+    def get_object(self):
+
+        product = get_object_or_404(
+            Product,
+            slug=self.kwargs.get("slug"),
+            is_available=True,
+        )
+
+        return product
+
+
+class ProductCreateView(
+    LoginRequiredMixin,
+    SellerRequiredMixin,
+    CreateView
+):
+
+    model = Product
+
+    form_class = ProductForm
+
+    template_name = "products/product_form.html"
+
+    success_url = reverse_lazy("products:product_list")
+
+    def form_valid(self, form):
+
+        form.instance.seller = self.request.user
+
+        return super().form_valid(form)
+
+
+class ProductUpdateView(
+    LoginRequiredMixin,
+    SellerRequiredMixin,
+    UpdateView
+):
+
+    model = Product
+
+    form_class = ProductForm
+
+    template_name = "products/product_form.html"
+
+    success_url = reverse_lazy("products:product_list")
+
+    def get_queryset(self):
+
+        return Product.objects.filter(
+            seller=self.request.user
+        )
+
+
+class ProductDeleteView(
+    LoginRequiredMixin,
+    SellerRequiredMixin,
+    DeleteView
+):
+
+    model = Product
+
+    template_name = "products/product_confirm_delete.html"
+
+    success_url = reverse_lazy("products:product_list")
+
+    def get_queryset(self):
+
+        return Product.objects.filter(
+            seller=self.request.user
+        )
